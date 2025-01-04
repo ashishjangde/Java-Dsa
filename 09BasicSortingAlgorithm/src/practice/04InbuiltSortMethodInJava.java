199fcd0eb6541be3f7fa3f8631401706b3b66535
Ashish Jangde <ashishjangde2003@gmail.com>
2025-01-04 23:21:28 +0530

    removed unused imports

--- 09BasicSortingAlgorithm/src/practice/04InbuiltSortMethodInJava.java
+++ 09BasicSortingAlgorithm/src/practice/04InbuiltSortMethodInJava.java
@@ -1,8 +1,7 @@
 package practice;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
+
 
 class InbuiltSortMethodInJava {
     public static void main(String[] args) {
